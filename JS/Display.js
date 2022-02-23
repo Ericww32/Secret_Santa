2b6b4eb5acce642c1c0608ebb05262fddb77c29c
Eric Willoughby <ericww32@yahoo.com>
2022-02-23 18:18:09 -0500

    added json back in directly for functional code and will have to come back to this idea later.

--- JS/Display.js
+++ JS/Display.js
@@ -4,73 +4,45 @@ let kidsDisplay = document.getElementById("kids");
 let babiesDisplay = document.getElementById("babies");
 
 // Pull in the JSON data
-fetch("JSON/adults.json")
-    .then(function(response) {
-        const adultsJSON = response;
-    })
-    .catch(function(err) {
-        console.log("Adults failed to load JSON data...");
-    });
+var adultsJSON = '{ ' +
+    '"person": [' +
+        '{"name": "Brenda", "assigned": "Cindy", "link": ""},' +
+        '{"name": "David", "assigned": "Jackie", "link": ""},' +
+        '{"name": "Jackie", "assigned": "Annette", "link": "https://www.amazon.com/hz/wishlist/ls/36UDARP2M10ZQ?ref_=wl_share"},' +
+        '{"name": "Sam", "assigned": "Joe", "link": ""},' +
+        '{"name": "Annette", "assigned": "David", "link": "https://www.amazon.com/hz/wishlist/ls/3PUOS221DGMJU?ref=cm_sw_sm_r_wl_ip_PLh48gOkiQzUJ"},' +
+        '{"name": "Joe", "assigned": "Sam", "link": ""},' +
+        '{"name": "Cindy", "assigned": "Brenda", "link": "https://www.amazon.com/hz/wishlist/ls/45542NYYP0KO?ref_=wl_share"}' +
+    ']' +
+'}';
 
-fetch("JSON/kids.json")
-    .then(function(response) {
-        const kidsJSON = response;
-    })
-    .catch(function(err) {
-        console.log("Kids failed to load JSON data...");
-    });
+var kidsJSON = '{ ' +
+    '"person": [' +
+        '{"name": "Eric", "assigned": "Morgan", "link": "https://www.amazon.com/hz/wishlist/ls/2TB6LH8QBNIRE?ref_=wl_share"},' +
+        '{"name": "Erin", "assigned": "Jeff", "link": ""},' +
+        '{"name": "Imran", "assigned": "Mikayla", "link": ""},' +
+        '{"name": "Jeff", "assigned": "Erin", "link": "https://www.amazon.com/hz/wishlist/ls/2BI9NMK0ZTYX3?ref_=wl_share"},' +
+        '{"name": "Melody", "assigned": "Imran", "link": "https://www.amazon.com/hz/wishlist/ls/RJX3PDJ6XB8S?ref_=wl_share"},' +
+        '{"name": "Mikayla", "assigned": "Melody", "link": "https://www.amazon.com/hz/wishlist/ls/2GJV6MB1EZB9Z?ref_=wl_share"},' +
+        '{"name": "Morgan", "assigned": "Eric", "link": "https://www.amazon.com/hz/wishlist/ls/RQ4O9T8XMS5?ref_=wl_share"}' +
+    ']' +
+'}';
 
-fetch("JSON/babies.json")
-    .then(function(response) {
-        const babiesJSON = response;
-    })
-    .catch(function(err) {
-        console.log("Babies failed to load JSON data...");
-    });
-
-const adultsJSON = require("../JSON/adults.json");
-const kidsJSON = require("../JSON/kids.json");
-const babiesJSON = require("../JSON/babies.json");
+var babiesJSON = '{ ' +
+    '"person": [' +
+        '{"name": "Aiden", "assigned": "Lilly", "link": ""},' +
+        '{"name": "Ayla", "assigned": "Henry", "link": ""},' +
+        '{"name": "Aslan", "assigned": "Izzy", "link": ""},' +
+        '{"name": "Henry", "assigned": "Mikey", "link": "https://www.amazon.com/hz/wishlist/ls/EYCB04U0H7MY/ref=nav_wishlist_lists_3?_encoding=UTF8&type=wishlist"},' +
+        '{"name": "Izzy", "assigned": "Aslan", "link": ""},' +
+        '{"name": "Lilly", "assigned": "Ayla", "link": "https://www.amazon.com/hz/wishlist/ls/3N3JBCVC6P85?ref=cm_sw_sm_r_wl_ip_UbTqwFHjZOtmH"},' +
+        '{"name": "Mikey", "assigned": "Aiden", "link": ""}' +
+    ']' +
+'}';
 
 console.log("Adults JSON: " + adultsJSON);
 console.log("Kids JSON: " + kidsJSON);
 console.log("Babies JSON: " + babiesJSON);
-
-// var adultsJSON = '{ ' +
-//     '"person": [' +
-//         '{"name": "Brenda", "assigned": "Cindy", "ignore": "", "link": ""},' +
-//         '{"name": "David", "assigned": "Jackie", "ignore": "", "link": ""},' +
-//         '{"name": "Jackie", "assigned": "Annette", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/36UDARP2M10ZQ?ref_=wl_share"},' +
-//         '{"name": "Sam", "assigned": "Joe", "ignore": "", "link": ""},' +
-//         '{"name": "Annette", "assigned": "David", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/3PUOS221DGMJU?ref=cm_sw_sm_r_wl_ip_PLh48gOkiQzUJ"},' +
-//         '{"name": "Joe", "assigned": "Sam", "ignore": "", "link": ""},' +
-//         '{"name": "Cindy", "assigned": "Brenda", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/45542NYYP0KO?ref_=wl_share"}' +
-//     ']' +
-// '}';
-//
-// var kidsJSON = '{ ' +
-//     '"person": [' +
-//         '{"name": "Eric", "assigned": "Morgan", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/2TB6LH8QBNIRE?ref_=wl_share"},' +
-//         '{"name": "Erin", "assigned": "Jeff", "ignore": "", "link": ""},' +
-//         '{"name": "Imran", "assigned": "Mikayla", "ignore": "", "link": ""},' +
-//         '{"name": "Jeff", "assigned": "Erin", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/2BI9NMK0ZTYX3?ref_=wl_share"},' +
-//         '{"name": "Melody", "assigned": "Imran", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/RJX3PDJ6XB8S?ref_=wl_share"},' +
-//         '{"name": "Mikayla", "assigned": "Melody", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/2GJV6MB1EZB9Z?ref_=wl_share"},' +
-//         '{"name": "Morgan", "assigned": "Eric", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/RQ4O9T8XMS5?ref_=wl_share"}' +
-//     ']' +
-// '}';
-//
-// var babiesJSON = '{ ' +
-//     '"person": [' +
-//         '{"name": "Aiden", "assigned": "Lilly", "ignore": "", "link": ""},' +
-//         '{"name": "Ayla", "assigned": "Henry", "ignore": "", "link": ""},' +
-//         '{"name": "Aslan", "assigned": "Izzy", "ignore": "", "link": ""},' +
-//         '{"name": "Henry", "assigned": "Mikey", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/EYCB04U0H7MY/ref=nav_wishlist_lists_3?_encoding=UTF8&type=wishlist"},' +
-//         '{"name": "Izzy", "assigned": "Aslan", "ignore": "", "link": ""},' +
-//         '{"name": "Lilly", "assigned": "Ayla", "ignore": "", "link": "https://www.amazon.com/hz/wishlist/ls/3N3JBCVC6P85?ref=cm_sw_sm_r_wl_ip_UbTqwFHjZOtmH"},' +
-//         '{"name": "Mikey", "assigned": "Aiden", "ignore": "", "link": ""}' +
-//     ']' +
-// '}';
 
 // Creating locally parsed JSON objects
 var adultsObj = JSON.parse(adultsJSON);

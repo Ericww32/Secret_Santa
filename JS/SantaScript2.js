8c1f5806d897282a4184d15a67261f3f72b35859
Eric Willoughby <ericww32@yahoo.com>
2022-12-09 15:17:52 -0500

    testing fetch json 8

--- JS/SantaScript2.js
+++ JS/SantaScript2.js
@@ -14,33 +14,33 @@ async function loadNames() {
 }
 
 // Grab the JSON objects and pass it to a variable
-let famJSON = JSON.stringify(loadNames());
+// let famJSON = JSON.stringify(loadNames());
 
-// let famJSON = '{ ' +
-//     '"person": [' +
-//     '{"name": "Brenda", "assigned": "Sam", "ignore": ["David", "Morgan", "Lilly", "Izzy", "Mikey"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2KVZA4434Y4LT"},' +
-//     '{"name": "David", "assigned": "Joe", "ignore": ["Brenda", "Morgan", "Lilly", "Izzy", "Mikey"], "history": "", "link": ""},' +
-//     '{"name": "Jackie", "assigned": "Brenda", "ignore": ["Sam", "Erin", "Imran", "Aidan", "Ayla", "Aslan"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/36UDARP2M10ZQ?ref_=wl_share"},' +
-//     '{"name": "Sam", "assigned": "Annette", "ignore": ["Jackie", "Erin", "Imran", "Aidan", "Ayla", "Aslan"], "history": "", "link": ""},' +
-//     '{"name": "Annette", "assigned": "Cindy", "ignore": ["Henry", "Melody", "Jeff", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/3PUOS221DGMJU?ref=cm_sw_sm_r_wl_ip_PLh48gOkiQzUJ"},' +
-//     '{"name": "Joe", "assigned": "David", "ignore": ["Cindy", "Mikayla"], "history": "", "link": ""},' +
-//     '{"name": "Cindy", "assigned": "Jackie", "ignore": ["Joe", "Mikayla"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/45542NYYP0KO?ref_=wl_share"},' +
-//     '{"name": "Eric", "assigned": "Imran", "ignore": ["Henry", "Annette", "Melody", "Jeff"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2TB6LH8QBNIRE?ref_=wl_share"},' +
-//     '{"name": "Erin", "assigned": "Morgan", "ignore": ["Imran", "Jackie", "Sam", "Aidan", "Ayla", "Aslan"], "history": "", "link": ""},' +
-//     '{"name": "Imran", "assigned": "Jeff", "ignore": ["Erin", "Jackie", "Sam", "Aidan", "Ayla", "Aslan"], "history": "", "link": ""},' +
-//     '{"name": "Jeff", "assigned": "Erin", "ignore": ["Henry", "Annette", "Melody", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2BI9NMK0ZTYX3?ref_=wl_share"},' +
-//     '{"name": "Melody", "assigned": "Mikayla", "ignore": ["Henry", "Annette", "Jeff", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/RJX3PDJ6XB8S?ref_=wl_share"},' +
-//     '{"name": "Mikayla", "assigned": "Eric", "ignore": ["Cindy", "Joe"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2GJV6MB1EZB9Z?ref_=wl_share"},' +
-//     '{"name": "Morgan", "assigned": "Melody", "ignore": ["David", "Brenda", "Lilly", "Izzy", "Mikey"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/RQ4O9T8XMS5?ref_=wl_share"},' +
-//     '{"name": "Aidan", "assigned": "Mikey", "ignore": ["Imran", "Erin", "Jackie", "Sam", "Ayla", "Aslan"], "history": "", "link": ""},' +
-//     '{"name": "Ayla", "assigned": "Izzy", "ignore": ["Aidan", "Imran", "Erin", "Jackie", "Sam", "Ayla", "Aslan"], "history": "", "link": ""},' +
-//     '{"name": "Aslan", "assigned": "Lilly", "ignore": ["Aidan", "Imran", "Erin", "Jackie", "Sam", "Ayla"], "history": "", "link": ""},' +
-//     '{"name": "Henry", "assigned": "Aidan", "ignore": ["Annette", "Melody", "Jeff", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/EYCB04U0H7MY/ref=nav_wishlist_lists_3?_encoding=UTF8&type=wishlist"},' +
-//     '{"name": "Izzy", "assigned": "Ayla", "ignore": ["David", "Morgan", "Lilly", "Brenda", "Mikey"], "history": "", "link": ""},' +
-//     '{"name": "Lilly", "assigned": "Henry", "ignore": ["David", "Morgan", "Brenda", "Izzy", "Mikey"], "history": "", "link": "https://www.amazon.com/registries/holiday/2HC3EM405FBX6/guest-view"},' +
-//     '{"name": "Mikey", "assigned": "Aslan", "ignore": ["David", "Morgan", "Lilly", "Izzy", "Brenda"], "history": "", "link": ""}' +
-//     ']' +
-// '}';
+let famJSON = '{ ' +
+    '"person": [' +
+    '{"name": "Brenda", "assigned": "Sam", "ignore": ["David", "Morgan", "Lilly", "Izzy", "Mikey"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2KVZA4434Y4LT"},' +
+    '{"name": "David", "assigned": "Joe", "ignore": ["Brenda", "Morgan", "Lilly", "Izzy", "Mikey"], "history": "", "link": ""},' +
+    '{"name": "Jackie", "assigned": "Brenda", "ignore": ["Sam", "Erin", "Imran", "Aidan", "Ayla", "Aslan"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/36UDARP2M10ZQ?ref_=wl_share"},' +
+    '{"name": "Sam", "assigned": "Annette", "ignore": ["Jackie", "Erin", "Imran", "Aidan", "Ayla", "Aslan"], "history": "", "link": ""},' +
+    '{"name": "Annette", "assigned": "Cindy", "ignore": ["Henry", "Melody", "Jeff", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/3PUOS221DGMJU?ref=cm_sw_sm_r_wl_ip_PLh48gOkiQzUJ"},' +
+    '{"name": "Joe", "assigned": "David", "ignore": ["Cindy", "Mikayla"], "history": "", "link": ""},' +
+    '{"name": "Cindy", "assigned": "Jackie", "ignore": ["Joe", "Mikayla"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/45542NYYP0KO?ref_=wl_share"},' +
+    '{"name": "Eric", "assigned": "Imran", "ignore": ["Henry", "Annette", "Melody", "Jeff"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2TB6LH8QBNIRE?ref_=wl_share"},' +
+    '{"name": "Erin", "assigned": "Morgan", "ignore": ["Imran", "Jackie", "Sam", "Aidan", "Ayla", "Aslan"], "history": "", "link": ""},' +
+    '{"name": "Imran", "assigned": "Jeff", "ignore": ["Erin", "Jackie", "Sam", "Aidan", "Ayla", "Aslan"], "history": "", "link": ""},' +
+    '{"name": "Jeff", "assigned": "Erin", "ignore": ["Henry", "Annette", "Melody", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2BI9NMK0ZTYX3?ref_=wl_share"},' +
+    '{"name": "Melody", "assigned": "Mikayla", "ignore": ["Henry", "Annette", "Jeff", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/RJX3PDJ6XB8S?ref_=wl_share"},' +
+    '{"name": "Mikayla", "assigned": "Eric", "ignore": ["Cindy", "Joe"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/2GJV6MB1EZB9Z?ref_=wl_share"},' +
+    '{"name": "Morgan", "assigned": "Melody", "ignore": ["David", "Brenda", "Lilly", "Izzy", "Mikey"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/RQ4O9T8XMS5?ref_=wl_share"},' +
+    '{"name": "Aidan", "assigned": "Mikey", "ignore": ["Imran", "Erin", "Jackie", "Sam", "Ayla", "Aslan"], "history": "", "link": ""},' +
+    '{"name": "Ayla", "assigned": "Izzy", "ignore": ["Aidan", "Imran", "Erin", "Jackie", "Sam", "Ayla", "Aslan"], "history": "", "link": ""},' +
+    '{"name": "Aslan", "assigned": "Lilly", "ignore": ["Aidan", "Imran", "Erin", "Jackie", "Sam", "Ayla"], "history": "", "link": ""},' +
+    '{"name": "Henry", "assigned": "Aidan", "ignore": ["Annette", "Melody", "Jeff", "Eric"], "history": "", "link": "https://www.amazon.com/hz/wishlist/ls/EYCB04U0H7MY/ref=nav_wishlist_lists_3?_encoding=UTF8&type=wishlist"},' +
+    '{"name": "Izzy", "assigned": "Ayla", "ignore": ["David", "Morgan", "Lilly", "Brenda", "Mikey"], "history": "", "link": ""},' +
+    '{"name": "Lilly", "assigned": "Henry", "ignore": ["David", "Morgan", "Brenda", "Izzy", "Mikey"], "history": "", "link": "https://www.amazon.com/registries/holiday/2HC3EM405FBX6/guest-view"},' +
+    '{"name": "Mikey", "assigned": "Aslan", "ignore": ["David", "Morgan", "Lilly", "Izzy", "Brenda"], "history": "", "link": ""}' +
+    ']' +
+'}';
 
 // Creating parsed JSON objects
 let famObj = JSON.parse(famJSON);
